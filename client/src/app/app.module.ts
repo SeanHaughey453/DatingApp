85d680136434c4ef212a6ccb7e7bd1a822ef36d3
AzureAD\SeanHaughey <sean.haughey@allsop.software.com>
2021-02-18 17:15:32 +0000

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 //import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -17,16 +23,19 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
-    
+    BrowserAnimationsModule,   
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
